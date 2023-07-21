03129b891e7d80456c9784fbc3d0dcea998f5bda
Dongheon Lee <dslisleedh@gmail.com>
2023-07-21 18:43:04 +0900

    Update download_df2k.sh

--- download_df2k.sh
+++ download_df2k.sh
@@ -35,30 +35,32 @@ echo "${RED}Downloading datasets... 8 / 8: DIV2K unknown LR x4${NC}"
 wget http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_train_LR_unknown_X4.zip
 wget http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_valid_LR_unknown_X4.zip
 
+mkdir -p ./DIV2K
+
 echo "${RED}Unzipping datasets...${NC}"
 echo "${RED}Flickr2K 1 / 8${NC}"
 tar -xvf Flickr2K.tar
 echo "${RED}DIV2K HR 2 / 8${NC}"
-unzip DIV2K_train_HR.zip
-unzip DIV2K_valid_HR.zip
+unzip DIV2K_train_HR.zip -d ./DIV2K/
+unzip DIV2K_valid_HR.zip -d ./DIV2K/
 echo "${RED}DIV2K bicubic LR x2 3 / 8${NC}"
-unzip DIV2K_train_LR_bicubic_X2.zip
-unzip DIV2K_valid_LR_bicubic_X2.zip
+unzip DIV2K_train_LR_bicubic_X2.zip -d ./DIV2K/
+unzip DIV2K_valid_LR_bicubic_X2.zip -d ./DIV2K/
 echo "${RED}DIV2K bicubic LR x3 4 / 8${NC}"
-unzip DIV2K_train_LR_bicubic_X3.zip
-unzip DIV2K_valid_LR_bicubic_X3.zip
+unzip DIV2K_train_LR_bicubic_X3.zip -d ./DIV2K/
+unzip DIV2K_valid_LR_bicubic_X3.zip -d ./DIV2K/
 echo "${RED}DIV2K bicubic LR x4 5 / 8${NC}"
-unzip DIV2K_train_LR_bicubic_X4.zip
-unzip DIV2K_valid_LR_bicubic_X4.zip
+unzip DIV2K_train_LR_bicubic_X4.zip -d ./DIV2K/
+unzip DIV2K_valid_LR_bicubic_X4.zip -d ./DIV2K/
 echo "${RED}DIV2K unknown LR x2 6 / 8${NC}"
-unzip DIV2K_train_LR_unknown_X2.zip
-unzip DIV2K_valid_LR_unknown_X2.zip
+unzip DIV2K_train_LR_unknown_X2.zip -d ./DIV2K/
+unzip DIV2K_valid_LR_unknown_X2.zip -d ./DIV2K/
 echo "${RED}DIV2K unknown LR x3 7 / 8${NC}"
-unzip DIV2K_train_LR_unknown_X3.zip
-unzip DIV2K_valid_LR_unknown_X3.zip
+unzip DIV2K_train_LR_unknown_X3.zip -d ./DIV2K/
+unzip DIV2K_valid_LR_unknown_X3.zip -d ./DIV2K/
 echo "${RED}DIV2K unknown LR x4 8 / 8${NC}"
-unzip DIV2K_train_LR_unknown_X4.zip
-unzip DIV2K_valid_LR_unknown_X4.zip
+unzip DIV2K_train_LR_unknown_X4.zip -d ./DIV2K/
+unzip DIV2K_valid_LR_unknown_X4.zip -d ./DIV2K/
 
 mkdir -p DF2K
 mkdir -p DF2K/DF2K_train_HR
@@ -72,18 +74,19 @@ mkdir -p DF2K/DF2K_train_LR_unknown/X3
 mkdir -p DF2K/DF2K_train_LR_unknown/X4
 
 echo "${RED}Moving datasets...${NC}"
-cp -r ./DIV2K_train_HR/* ./DF2K/DF2K_train_HR
-cp -r ./Flickr2K/Flickr2K_HR/* ./DF2K/DF2K_train_HR
-cp -r ./DIV2K_train_LR_bicubic/X2/ ./DF2K/DF2K_train_LR_bicubic
-cp -r ./DIV2K_train_LR_bicubic/X3/ ./DF2K/DF2K_train_LR_bicubic
-cp -r ./DIV2K_train_LR_bicubic/X4/ ./DF2K/DF2K_train_LR_bicubic
-cp -r ./DIV2K_train_LR_unknown/X2/ ./DF2K/DF2K_train_LR_unknown
-cp -r ./DIV2K_train_LR_unknown/X3/ ./DF2K/DF2K_train_LR_unknown
-cp -r ./DIV2K_train_LR_unknown/X4/ ./DF2K/DF2K_train_LR_unknown
-cp -r ./Flickr2K/Flickr2K_LR_bicubic/X2/ ./DF2K/DF2K_train_LR_bicubic
-cp -r ./Flickr2K/Flickr2K_LR_bicubic/X3/ ./DF2K/DF2K_train_LR_bicubic
-cp -r ./Flickr2K/Flickr2K_LR_bicubic/X4/ ./DF2K/DF2K_train_LR_bicubic
+cp -vr ./DIV2K/DIV2K_train_HR/* ./DF2K/DF2K_train_HR
+cp -vr ./Flickr2K/Flickr2K_HR/* ./DF2K/DF2K_train_HR
+cp -vr ./DIV2K/DIV2K_train_LR_bicubic/X2/ ./DF2K/DF2K_train_LR_bicubic
+cp -vr ./DIV2K/DIV2K_train_LR_bicubic/X3/ ./DF2K/DF2K_train_LR_bicubic
+cp -vr ./DIV2K/DIV2K_train_LR_bicubic/X4/ ./DF2K/DF2K_train_LR_bicubic
+cp -vr ./DIV2K/DIV2K_train_LR_unknown/X2/ ./DF2K/DF2K_train_LR_unknown
+cp -vr ./DIV2K/DIV2K_train_LR_unknown/X3/ ./DF2K/DF2K_train_LR_unknown
+cp -vr ./DIV2K/DIV2K_train_LR_unknown/X4/ ./DF2K/DF2K_train_LR_unknown
+cp -vr ./Flickr2K/Flickr2K_LR_bicubic/X2/ ./DF2K/DF2K_train_LR_bicubic
+cp -vr ./Flickr2K/Flickr2K_LR_bicubic/X3/ ./DF2K/DF2K_train_LR_bicubic
+cp -vr ./Flickr2K/Flickr2K_LR_bicubic/X4/ ./DF2K/DF2K_train_LR_bicubic
 
 echo "${RED}Removing zips ...${NC}"
 rm -rf ./*.zip
 rm -rf ./*.tar
+
